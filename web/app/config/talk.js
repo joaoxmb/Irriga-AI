b5e76b30acc14f09d63b6063a03543a90635803f
jaoxmb <joaovictorcgdossantos@gmail.com>
2023-12-02 12:55:07 -0300

    Chore: prompt

--- web/app/config/talk.js
+++ web/app/config/talk.js
@@ -6,7 +6,7 @@ let PARAMS = {};
 let SYSTEM = {};
 
 async function definirParametros() {
-  const prompt = [
+  const promptPrimary = [
     {
       "role": "system",
       "content": "Você receberá o nome de uma muda em português e receberá o nome de uma cidade."
@@ -22,36 +22,52 @@ async function definirParametros() {
     {
       "role": "system",
       "content": "Agora defina os seguintes parametros: STATUS: com base na planta que foi passada pelo usuario, se ela for realmente uma planta verdadeira coloque OK, caso contrario ERROR. MENSAGEM: descreva o porque de OK ou ERROR. PLANTA: escreva o nome da planta com o primeiro caractere maiúsculo. SOBRE: escreva um texto de 10 linhas sobre a planta e suas características. MAX-UMIDADE: escreva um valor de 0 a 100 que represente a umidade maxima do solo suportada pela planta levando em consideração sua necessidade hídrica, esse valor não pode ser superior doque 90. MIN-UMIDADE: escreva um valor de 0 a 100 que represente a umidade minima do solo suportada pela planta levando em consideração sua necessidade hídrica, esse valor não pode ser abaixo de 10. SEMANA: crie um plano semanal de rega para a planta com base em sua necessidade hidrica. Insira ele dentro de um array de boleanos, o array começa pelo domingo, coloque true se for para regar e false caso contrario, repita para os 7 dias da semana, o ultimo valor deverá ser sempre true. MODO-SEGURANCA: se a planta sofrer algum tipo de dano caso a umidade do solo fique abaixo de 5% coloque true, caso contrario false. Geralmente espécies de cactos não sofrem com essa baixa umidade. MODO-SEGURANCA-MOTIVO: descreva o motivo pelo qual você tomou a decisao ao definir MODO-SEGURANCA. CIDADE: escreva o nome da cidade formatado e a latitude e longitude da cidade em um array, a cidade será passada pelo usuario. TEMPERATURA: escreva a temperatura minima e temperatura maxima em um array que seja ideal para a planta passada pelo usuario. TEMPERATURA-DICAS: escreva uma mensagem para quando a temperatura estiver abaixo do limite para a planta, quando a temperatura estiver adequado para a planta e quando a temperatura estiver a mais para a planta em um array crescente."
+    }
+  ]
+  const promptSecondary = [
+    {
+      "role": "system",
+      "content": ""
     },
     {
       "role": "system",
       "content": "Retorne o seguinte JSON com os resultados: { \"umidade\": { \"min\": MIN-UMIDADE, \"max\": MAX-UMIDADE }, \"planta\": { \"nome\": PLANTA, \"sobre\": SOBRE }, \"cidade\": { \"nome\": CIDADE>NOME, \"coordenadas\": [CIDADE>LATITUDE, CIDADE>LONGITUDE] }, \"seguranca\": [ MODO-SEGURANCA, MODO-SEGURANCA-MOTIVO ], \"semana\": [SEMANA], \"temperatura\": { \"graus\": [TEMPERATURA>MINIMA, TEMPERATURA>MAXIMA], \"dicas\": [TEMPERATURA-DICAS] }, \"status\": STATUS, \"mensagem\": MENSAGEM }"
     }
   ]
-
-  await aiRequest(prompt)
+  await aiRequest(promptPrimary)
     .then(async (data) => {
-      PARAMS = {...data};
+      promptSecondary[0].content = data;
 
-      console.log(PARAMS);
-      PARAMS["semana"][6] = true;
-      
-      await chat("ia", { text: "Tudo certo! Já defini todos os parâmetros." })
-      await chat("ia", { text: `Com base na necessidade hídrica da ${PARAMS.planta.nome}, cheguei à conclusão que a umidade do solo ideal é de ${PARAMS.umidade.min}% a ${PARAMS.umidade.max}%`, delay: 2000 })
-      await chat("ia", { text: `Aqui está os dias da semana que será feito a rega:` })
-      await chat("ia", {
-        text: `${PARAMS.semana
-          .reduce((p, c, index) => {
-            const week = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado"];
-
-            p += `${c ? "✔️" : "❌"} ${week[index]}\n`
-            return p
-          }, "")
-          }`,
-        delay: 2000
+      await aiRequest(promptSecondary)
+        .then(async (data) => {
+          PARAMS = {...data};
+    
+          console.log(PARAMS);
+          PARAMS["semana"][6] = true;
+          
+          await chat("ia", { text: "Tudo certo! Já defini todos os parâmetros." })
+          await chat("ia", { text: `Com base na necessidade hídrica da ${PARAMS.planta.nome}, cheguei à conclusão que a umidade do solo ideal é de ${PARAMS.umidade.min}% a ${PARAMS.umidade.max}%`, delay: 2000 })
+          await chat("ia", { text: `Aqui está os dias da semana que será feito a rega:` })
+          await chat("ia", {
+            text: `${PARAMS.semana
+              .reduce((p, c, index) => {
+                const week = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado"];
+    
+                p += `${c ? "✔️" : "❌"} ${week[index]}\n`
+                return p
+              }, "")
+              }`,
+            delay: 2000
+          })
+          await chat("ia", {text: PARAMS.planta.sobre})
+        })
+        .catch(async (error) => {
+          // await chat("ia", { text: error.mensagem || "Error" })
+          await chat("ia", { text: "Desculpe, terei que pedir que preencha novamente o campo de qual planta estaremos regando.", delay: 1000 })
+          await perguntarPlanta()
+          await definirParametros()
+        })
       })
-      await chat("ia", {text: PARAMS.planta.sobre})
-    })
     .catch(async (error) => {
       // await chat("ia", { text: error.mensagem || "Error" })
       await chat("ia", { text: "Desculpe, terei que pedir que preencha novamente o campo de qual planta estaremos regando.", delay: 1000 })
